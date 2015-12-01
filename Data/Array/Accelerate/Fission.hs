0cead5b37ff73ddab4083bfe0466d12c4327b508
Ryan Newton <rrnewton@gmail.com>
2015-12-01 14:21:54 -0500

    Reorder defs in Fission.hs so I can load just up thru map

--- Data/Array/Accelerate/Fission.hs
+++ Data/Array/Accelerate/Fission.hs
@@ -37,23 +37,91 @@ instance A.Arrays a => Show (Rep a) where
 type DimId = Int
 
 
+--------------------------------------------------------------------------------
+-- Utilities
+
+split :: (A.Slice sh,Shape sh,Elt a)
+      => DimId
+      -> A.Acc (Array (sh A.:. Int) a)
+      -> TuneM ( A.Acc (Array (sh A.:. Int) a),
+                 A.Acc (Array (sh A.:. Int) a))
+split _dimid arr = return (arr1, arr2)
+    where arrTl = A.indexTail $ A.shape arr
+          arrHd = A.indexHead $ A.shape arr
+          (chunk, leftover) = arrHd `quotRem` 2
+          arr1Sh = arrTl :. chunk
+          arr2Sh = arrTl :. (chunk + leftover)
+          adjust i = let t = A.indexTail i
+                         h = A.indexHead i
+                     in A.lift $ t :. (h + chunk)
+          arr1 = A.generate (A.lift arr1Sh) (\sh -> arr A.! sh)
+          arr2 = A.generate (A.lift arr2Sh) (\sh -> arr A.! (adjust sh))
+
+    --   return (splitArray (A.constant 0), splitArray (A.constant 1))
+    -- where splitArray i =
+    --           let shead             = A.indexHead $ A.shape arr
+    --               (chunk, leftover) = shead `quotRem` 2
+    --               start             = (i A.<* leftover) A.?
+    --                                   (i * (chunk + 1),
+    --                                    i * chunk + leftover)
+    --               end               = ((i+1) A.<* leftover) A.?
+    --                                   (start + chunk,
+    --                                    (i+1) * chunk + leftover)
+    --               bsh               = A.lift $ (A.indexTail $ A.shape arr) A.:. (end - start)
+    --               f x               = A.lift $ (A.indexTail x) A.:. ((A.indexHead x) + start)
+    --           in A.backpermute bsh f arr
+
+concat :: DimId -> Rep a -> Rep a -> TuneM (Rep a)
+concat d3 (Concat d1 ls1) (Concat d2 ls2)
+ | d1 == d2 && d1 == d3 = return $ Concat d3 (ls1 ++ ls2)
+ | otherwise = error "Brain explodes for now..."
+
+askTuner :: [Int] -> TuneM Int
+askTuner ls = return $ head ls
+
+matchArrayShape
+    :: forall acc aenv sh sh' e. (Shape sh, Shape sh')
+    => {- dummy -} acc aenv (Array sh e)
+    -> {- dummy -} sh'
+    -> Maybe (sh :=: sh')
+matchArrayShape _ _
+  | Just REFL <- matchTupleType (eltType (undefined::sh)) (eltType (undefined::sh'))
+  = gcast REFL
+
+  | otherwise
+  = Nothing
+
+matchShape
+  :: forall sh sh'. (Shape sh, Shape sh')
+  => sh
+  -> sh'
+  -> Maybe (sh :=: sh')
+matchShape _ _
+  | Just REFL <- matchTupleType (eltType (undefined::sh))
+                                (eltType (undefined::sh'))
+  = gcast REFL
+
+  | otherwise
+  = Nothing
+
+matchSlice --- ?????
+  :: forall sh sh'. (Slice sh, Slice sh')
+  => sh
+  -> sh'
+  -> Maybe (sh :=: sh')
+matchSlice _ _
+  | Just REFL <- matchTupleType (eltType (undefined::sh))
+                                (eltType (undefined::sh'))
+  = gcast REFL
+
+  | otherwise
+  = Nothing
+
 mkacc a = MkAcc $ Concat 0 [a]
 
-combine
-  :: (Slice sh, Shape sh, Elt e) =>
-     Acc (Array (sh :. Int) e) -> A.Acc (Array (sh :. Int) e)
-combine (MkAcc (Concat 0 [a])) = a
-combine (MkAcc (Concat 0 as)) = foldr1 (A.++) $ P.map A.compute as
-
-combine0 (MkAcc (Concat 0 [a])) = a
-
-combine'
-  :: (Slice sh, Shape sh, Elt e) =>
-     Acc (Array (sh :. Int) e) -> A.Acc (Array (sh :. Int) e)
-combine' (MkAcc (Concat 0 [a])) = a
-combine' (MkAcc (Concat 0 as)) = foldr1 (A.++) as
-
-
+--------------------------------------------------------------------------------
+-- RUNNING
+--------------------------------------------------------------------------------
 
 go1 :: (Slice sh, Shape sh, Elt a)
     => (forall arrs. Arrays arrs => A.Acc arrs -> arrs)
@@ -85,13 +153,10 @@ run exec arrs
   | Just REFL <- matchShape (undefined :: DIM2) (undefined :: sh) = go1 exec arrs
   | otherwise                                                     = go0 exec arrs
 
-sfoldl :: forall sh a b. (Shape sh, Slice sh, Elt a, Elt b)
-       => (A.Exp a -> A.Exp b -> A.Exp a)
-       -> A.Exp a
-       -> A.Exp sh
-       -> Acc (Array (sh :. Int) b)
-       -> A.Exp a
-sfoldl = undefined
+
+--------------------------------------------------------------------------------
+-- Wrappers for Core Accelerate operations
+--------------------------------------------------------------------------------
 
 map1n :: (Slice ix, Shape ix, Elt a, Elt b) =>
          (A.Exp a -> A.Exp b) -> Acc (Array (ix :. Int) a) -> Int ->
@@ -121,6 +186,7 @@ map f arr
     | Just REFL <- matchShape (undefined :: A.DIM1) (undefined :: ix) = map1n f arr 1
     | Just REFL <- matchShape (undefined :: A.DIM2) (undefined :: ix) = map1n f arr 2
     | otherwise = map0 f arr
+
 
 -- Doesn't work
 -- replicaten :: (Slice slix, Elt e)
@@ -308,84 +374,28 @@ zipWith f (MkAcc (Concat d1 [m1])) (MkAcc (Concat d2 [m21,m22])) =
      return $ MkAcc $ Concat d1 [m1',m2']
 zipWith _ _ _ = error "Not implemented"
 
-split :: (A.Slice sh,Shape sh,Elt a)
-      => DimId
-      -> A.Acc (Array (sh A.:. Int) a)
-      -> TuneM ( A.Acc (Array (sh A.:. Int) a),
-                 A.Acc (Array (sh A.:. Int) a))
-split _dimid arr = return (arr1, arr2)
-    where arrTl = A.indexTail $ A.shape arr
-          arrHd = A.indexHead $ A.shape arr
-          (chunk, leftover) = arrHd `quotRem` 2
-          arr1Sh = arrTl :. chunk
-          arr2Sh = arrTl :. (chunk + leftover)
-          adjust i = let t = A.indexTail i
-                         h = A.indexHead i
-                     in A.lift $ t :. (h + chunk)
-          arr1 = A.generate (A.lift arr1Sh) (\sh -> arr A.! sh)
-          arr2 = A.generate (A.lift arr2Sh) (\sh -> arr A.! (adjust sh))
 
-    --   return (splitArray (A.constant 0), splitArray (A.constant 1))
-    -- where splitArray i =
-    --           let shead             = A.indexHead $ A.shape arr
-    --               (chunk, leftover) = shead `quotRem` 2
-    --               start             = (i A.<* leftover) A.?
-    --                                   (i * (chunk + 1),
-    --                                    i * chunk + leftover)
-    --               end               = ((i+1) A.<* leftover) A.?
-    --                                   (start + chunk,
-    --                                    (i+1) * chunk + leftover)
-    --               bsh               = A.lift $ (A.indexTail $ A.shape arr) A.:. (end - start)
-    --               f x               = A.lift $ (A.indexTail x) A.:. ((A.indexHead x) + start)
-    --           in A.backpermute bsh f arr
+combine
+  :: (Slice sh, Shape sh, Elt e) =>
+     Acc (Array (sh :. Int) e) -> A.Acc (Array (sh :. Int) e)
+combine (MkAcc (Concat 0 [a])) = a
+combine (MkAcc (Concat 0 as)) = foldr1 (A.++) $ P.map A.compute as
 
-concat :: DimId -> Rep a -> Rep a -> TuneM (Rep a)
-concat d3 (Concat d1 ls1) (Concat d2 ls2)
- | d1 == d2 && d1 == d3 = return $ Concat d3 (ls1 ++ ls2)
- | otherwise = error "Brain explodes for now..."
+combine0 (MkAcc (Concat 0 [a])) = a
 
-askTuner :: [Int] -> TuneM Int
-askTuner ls = return $ head ls
+combine'
+  :: (Slice sh, Shape sh, Elt e) =>
+     Acc (Array (sh :. Int) e) -> A.Acc (Array (sh :. Int) e)
+combine' (MkAcc (Concat 0 [a])) = a
+combine' (MkAcc (Concat 0 as)) = foldr1 (A.++) as
 
-
-matchArrayShape
-    :: forall acc aenv sh sh' e. (Shape sh, Shape sh')
-    => {- dummy -} acc aenv (Array sh e)
-    -> {- dummy -} sh'
-    -> Maybe (sh :=: sh')
-matchArrayShape _ _
-  | Just REFL <- matchTupleType (eltType (undefined::sh)) (eltType (undefined::sh'))
-  = gcast REFL
-
-  | otherwise
-  = Nothing
-
-matchShape
-  :: forall sh sh'. (Shape sh, Shape sh')
-  => sh
-  -> sh'
-  -> Maybe (sh :=: sh')
-matchShape _ _
-  | Just REFL <- matchTupleType (eltType (undefined::sh))
-                                (eltType (undefined::sh'))
-  = gcast REFL
-
-  | otherwise
-  = Nothing
-
-matchSlice --- ?????
-  :: forall sh sh'. (Slice sh, Slice sh')
-  => sh
-  -> sh'
-  -> Maybe (sh :=: sh')
-matchSlice _ _
-  | Just REFL <- matchTupleType (eltType (undefined::sh))
-                                (eltType (undefined::sh'))
-  = gcast REFL
-
-  | otherwise
-  = Nothing
-
+sfoldl :: forall sh a b. (Shape sh, Slice sh, Elt a, Elt b)
+       => (A.Exp a -> A.Exp b -> A.Exp a)
+       -> A.Exp a
+       -> A.Exp sh
+       -> Acc (Array (sh :. Int) b)
+       -> A.Exp a
+sfoldl = undefined
 
 
 -- TESTS
@@ -406,4 +416,3 @@ matchSlice _ _
 -- a2' = do { a1' <- a1; a2' <- Fission1.map (* 2) a1'; return $ combine a2' }
 -- a3' = do { a2' <- a2; a3' <- Fission1.fold1 (+) a2'; return $ combine0 a3' }
 -- a4' = do { a1' <- a1; a2' <- a2; a4' <- Fission1.zipWith (+) a1' a2'; return $ combine a4' }
-
