281cebe1be6ddfd5a2919e235f6b564d636873bf
Michael Vollmer <vollmerm@indiana.edu>
2016-02-12 10:38:21 -0500

    Revert "Remove extra type parameters."
    This reverts commit 74704a86ed580a4dbc799f242c16568f0ed88a8e.
    
    Committed it before I tested it.

--- Data/Array/Accelerate/Fission/Runner.hs
+++ Data/Array/Accelerate/Fission/Runner.hs
@@ -49,16 +49,16 @@ import           Numeric.Natural
 
 -- | The language of multi-device computations.
 
-data Rep a = Concat [a]
-           | Split a
+data Rep c a = Concat c [a]
+             | Split c a
 
-newtype Wrap a m = MkWrap (Natural -> Exec -> m (Rep a))
+newtype Wrap c a m = MkWrap (Natural -> Exec a -> m (Rep c a))
 
 type TuneM a = ReaderT [(String,Int)] IO a
 
-type Acc a = Wrap (A.Acc a) (ReaderT [(String,Int)] IO)
+type Acc a = Wrap SplitBy (A.Acc a) (ReaderT [(String,Int)] IO)
 
-type Exec = forall a. (Arrays a) => (A.Acc a) -> a
+type Exec a = forall a. (Arrays a) => (A.Acc a) -> a
 
 --mkacc :: (Natural -> ((A.Acc a) -> b -> a) -> TuneM (Rep Devs SplitBy (A.Acc a))) -> Acc a
 --mkacc = MkWrap
@@ -66,12 +66,12 @@ type Exec = forall a. (Arrays a) => (A.Acc a) -> a
 type SplitBy = Int
 type DimId = SplitBy
 
-instance (Show a, A.Arrays a) => Show (Rep (A.Acc a)) where
-  show (Concat ls) =
-      "(Concat of "++ show (length ls)++" chunks:\n" ++
+instance (Show a, A.Arrays a) => Show (Rep Int (A.Acc a)) where
+  show (Concat d ls) =
+      "(Concat along dim "++ show d++" of "++ show (length ls)++" chunks:\n" ++
                          unlines [ show x | x <- ls ]++")"
-  show (Split a) =
-     "(Split of "++ show a++")"
+  show (Split d a) =
+     "(Split along dim "++show d++ " of "++ show a++")"
 
 runTune :: TuneM a -> IO a
 runTune f = runReaderT f [("split",2)]
@@ -105,10 +105,11 @@ go1 exec (MkWrap fn) =
    do rep <- runTune $ fn 2 $ \a -> exec a
       putStrLn ("Fission/RUN1: shallow-language term:\n" ++ show rep)
       case rep of
-        (Concat arrs)
+        (Concat dim arrs)
           | null arrs    -> error "Data.Array.Accelerate.Fusion.go1: nothing to do"
+          | dim /= 0     -> error "Data.Array.Accelerate.Fusion.go1: I only know about dimension 0"
           | otherwise    -> return $! P.map exec arrs
-        (Split arr) -> return $! [exec arr]
+        (Split _dim arr) -> return $! [exec arr]
 
 go0 :: (Shape sh, Elt a)
     => (forall arrs. Arrays arrs => A.Acc arrs -> arrs)
@@ -119,11 +120,11 @@ go0 exec (MkWrap fn) =
   do rep <- runTune $ fn 2 $ \a -> exec a
      putStrLn ("Fission/RUN0: shallow-language term:\n" ++ show rep)
      case rep of
-      (Concat arrs)
+      (Concat _ arrs)
         | null arrs    -> error "Data.Array.Accelerate.Fusion.go0: nothing to do"
         | [a] <- arrs  -> return $! [exec a]
         | otherwise    -> error "Data.Array.Accelerate.Fusion.go0: not implemented yet"
-      (Split arr) -> return $! [exec arr]
+      (Split _dim arr) -> return $! [exec arr]
 
 
 run :: forall sh a. (Slice sh, Shape sh, Elt a)
@@ -144,7 +145,7 @@ run' arrs = run B.run arrs
 use :: Arrays arrays => (arrays,arrays) -> Acc arrays
 use (a,b) = MkWrap $ \numSplits _runner ->
             do case numSplits of
-                 2 -> return $ Concat [A.use a, A.use b]
+                 2 -> return $ Concat 0 [A.use a, A.use b]
                  _ -> error "Data.Array.Accelerate.Fusion.use: not handled yet"
 
 
@@ -152,4 +153,4 @@ use (a,b) = MkWrap $ \numSplits _runner ->
 -- FIXME: This should probably introduce a split node.
 --liftAcc :: A.Acc a -> Acc a
 liftAcc :: A.Acc a -> Acc a
-liftAcc a = MkWrap $ \_ _ -> return $ Concat [a]
+liftAcc a = MkWrap $ \_ _ -> return $ Concat 0 [a]
