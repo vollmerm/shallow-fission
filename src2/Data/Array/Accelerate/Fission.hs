b526f9a965c0764cfbba529985b42976f8dbc8e5
Michael Vollmer <vollmerm@indiana.edu>
2016-02-27 19:30:57 -0500

    Running into roadblock with types for interpreter

--- src2/Data/Array/Accelerate/Fission.hs
+++ src2/Data/Array/Accelerate/Fission.hs
@@ -37,6 +37,7 @@ data FAcc a where
 
 type Acc a = Rep (FAcc a)
 
+
 fizzMap :: (Shape sh, Elt a, Elt b) =>
            (Exp a -> Exp b)
         -> Acc (Array sh a)
@@ -107,27 +108,22 @@ arr = Use $ FAcc 0 [use $ A.fromList (Z :. 10 :. 10) [0..],
 
 foo1 :: (Shape sh) => Acc (Array sh Float) -> Acc (Array sh Float)
 foo1 as = fizzMap (+ 1) as
--- Return map
+-- Bind (Use a) (\a -> Return (map (+ 1) a))
 
 foo2 :: Shape sh => Acc (Array sh Float) -> Acc (Array sh Float)
 foo2 as = fizzMap (* 2) (foo1 as)
---foo2 as = Bind (foo1 as) $ fizzMap (* 2)
--- Bind (Return map) map
+-- Bind (Bind (Use a) (\a -> Return (map (+ 1) a))) (\a -> (Return map (* 2) a))
 
 foo3 :: Shape sh => Acc (Array sh Float) -> Acc (Array sh (Float,Float))
 foo3 as = fizzMap (\x -> A.lift (x,1::Float)) (foo2 as)
---foo3 as = Bind (foo2 as) $ fizzMap (\x -> A.lift (x,1::Float))
--- Bind (Bind (Return map) map) map
+-- Bind (Bind (Bind (Use a) (\a -> Return (map (+ 1) a))) (\a -> (Return map (* 2) a))) (\a -> Return (map f a))
 
 foo4 :: Shape sh => Acc (Array (sh :. Int) Float) -> Acc (Array sh Float)
 foo4 as = fizzFold (+) 0 (foo2 as)
---foo4 as = Bind (foo2 as) $ fizzFold (+) 0
--- Bind (Bind (Return map) map) (Bind fold (Join zipwith))
+-- Bind (Bind (Bind (Bind (Use a) (\a -> Return (map (+ 1) a))) (\a -> (Return map (* 2) a))) (\a -> Return (map f a))) (\a -> Bind (Return fold a) (\a -> Join a))
 
 foo5 :: Shape sh => Acc (Array (sh :. Int) Float) -> Acc (Array sh Float)
 foo5 as = fizzMap (* 5) (foo4 as)
---foo5 as = Bind (foo4 as) $ fizzMap (* 5)
--- Bind (Bind (Bind (Return map) map) (Bind fold (Join zipwith))) map
 
 naiveEval :: Rep a -> a
 naiveEval (Return a)   = a
@@ -135,193 +131,37 @@ naiveEval (Bind b f)   = naiveEval $ f (naiveEval b) --  $ Acc 0 $ fizzCompute (
 naiveEval (Join f a b) = naiveEval $ f (naiveEval a) (naiveEval b)
 naiveEval (Use a)      = a
 
--- fizzCompute :: forall a. Arrays a => Acc a -> Acc a
--- fizzCompute (FAcc s as) = FAcc s $ P.map (A.compute) as
+-- Can't write this:
+-- computeEval :: Rep (FAcc a) -> FAcc a
+-- computeEval (Return a)   = a
+-- computeEval (Bind b f)   = computeEval $ f $ fizzCompute (computeEval b)
+-- computeEval (Join f a b) = computeEval $ f (computeEval a) (computeEval b)
+-- computeEval (Use a)      = a
+
+
+fizzCompute :: forall a. Arrays a => FAcc a -> FAcc a
+fizzCompute (FAcc s as) = FAcc s $ P.map (A.compute) as
 
 -- λ> naiveEval $ foo1 arr
 -- Acc 0 [let a0 = use (Array (Z :. 10) [0.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0])
 -- in map (\x0 -> 1.0 + x0) a0]
 
 
-simplify :: Rep a -> Rep a
-simplify (Return a) = Return a
-simplify (Bind b f) = simplifyFunc f (simplify b)
-simplify (Join f a b) = Join f (simplify a) (simplify b)
-simplify (Use a) = Use a
-
-simplifyFunc :: (b -> Rep a) -> Rep b -> Rep a
-simplifyFunc f (Return a) = f a
-simplifyFunc f (Bind b g) =
-    let b' = simplify b
+partialEval :: Rep a -> Rep a
+partialEval (Return a) = Return a
+partialEval (Bind b f) =
+    let b' = partialEval b
     in case b' of
-         Return a -> simplifyFunc f (g a)
-         Use a -> Bind (Use a) (\a' -> Bind (g a') f)
-         _ -> Bind (Bind b' g) f
-simplifyFunc f (Join g a b) =
-    let a' = simplify a
-        b' = simplify b
-    in Bind (Join g a' b') f
-simplifyFunc f (Use a) =
-    Bind (Use a) f
+         Return a -> f a
+         Use a -> Bind (Use a) f
+         Bind b'' g -> Bind b'' $ repCompose g f
+partialEval (Join f a b) = Join f (partialEval a) (partialEval b)
+partialEval (Use a) = Use a
 
--- simplifyFunc f (Join g a b) =
---     let a' = simplify a
---         b' = simplify b
---     in case (a',b') of
---          (Return a, Return b) -> Bind (g a b) f
---          _ -> Bind (Join g a' b') f
-
--- data F a where
---   Do   :: a -> F a
---   Join :: (a -> b -> t (F c)) -> F a -> F b -> F c
-
-
--- data Acc a where
---   Acc :: Int -> [A.Acc a] -> Acc a
---   -- One  :: A.Acc a -> Acc a
---   -- Many :: Int -> [A.Acc a] -> Acc a
-
-
--- amap :: (A.Acc a -> A.Acc b) -> Acc a -> Acc b
--- amap f (Acc s as) = Acc s (P.map f as)
-
-
--- map' :: (MonadPlus t, Shape sh, Elt a, Elt b)
---      => (Exp a -> Exp b)
---      -> Acc (Array sh a)
---      -> t (F (Acc (Array sh b)))
--- map' f as = return $ Do $ amap (A.map f) as
-
---   -- = return . Do $ Acc s (fmap (A.map f) as)
-
---   -- One a       -> return . Do $ One (A.map f a)
---   -- Many dim as -> return . Do $ Many dim (fmap (A.map f) as)
-
---   -- return $ Do (P.map (A.map f) as)
-
-
-
--- fold' :: forall t sh e. (MonadPlus t, Shape sh, Elt e)
---       => (Exp e -> Exp e -> Exp e)
---       -> Exp e
---       -> Acc (Array (sh :. Int) e)
---       -> t (F (Acc (Array sh e)))
--- fold' f z as@(Acc dim _) =
---   let as' = amap (A.fold f z) as
---   in case dim of
---        0 -> join0 f as'
---        1 -> concatV as'
---        _ -> mzero
-
-
--- concatV
---     :: (MonadPlus t, Shape sh, Elt e)
---     => Acc (Array sh e)
---     -> t (F (Acc (Array sh e)))
--- concatV = undefined
-
--- join0 :: (MonadPlus t, Shape sh, Elt e)
---       => (Exp e -> Exp e -> Exp e)
---       -> Acc (Array sh e)
---       -> t (F (Acc (Array sh e)))
--- join0 = undefined
--- -- join0 f = return $ P.foldl1 (Join (zipWith' f)) . P.map (return . Do) . fanout
-
--- fanout :: Acc a -> [Acc a]
--- fanout (Acc s as) = P.map (\a -> Acc s [a]) as
-
-
--- {--
---   where
---     fold'0 f z [a]    = Do $ Acc 0 [A.fold f z a]
---     fold'0 f z (a:as) =
---       Join (fold'0 f z [a]) (fold'0 f z as)
---            (zipWith' f :: Acc (Array sh e) -> Acc (Array sh e) -> t (F (Acc (Array sh e))))
-
---     fold'1 f z [a]    = Do $ Acc 1 [A.fold f z a]
---     fold'1 f z (a:as) = error "TODO: fold on non-zero split dimension"
--- --}
-
-
--- zipWith'
---     :: (MonadPlus t, Shape sh, Elt a, Elt b, Elt c)
---     => (Exp a -> Exp b -> Exp c)
---     -> Acc (Array sh a)
---     -> Acc (Array sh b)
---     -> t (F (Acc (Array sh c)))
-
--- zipWith' f (Acc s1 a1) (Acc s2 a2) = do
---   guard (s1 == s2)
---   guard (P.length a1 == P.length a2)
---   return $ Do $ Acc s1 (P.zipWith (A.zipWith f) a1 a2)
-
-
-
-
-
-
-
-
-
--- -- data Request a where
--- --     Request :: [Par a] -> Request [a]
-
--- -- data Par a where
--- --     Do   :: a -> Par a
--- --     Par  :: Par a -> Par b -> Par (a,b)
--- --     Join :: Par a -> Par b -> (a -> b -> Par c) -> Par c
-
-
--- -- type Acc a = [Par (A.Acc a)]
-
-
--- -- use :: Arrays arrs => arrs -> Acc arrs
--- -- use = return . Do . A.use
-
--- -- map :: (Shape sh, Elt a, Elt b)
--- --     => (Exp a -> Exp b)
--- --     -> Acc (Array sh a)
--- --     -> Acc (Array sh b)
-
--- {--
--- map f xs = do
---   xs' <- tune xs
---   return (P.map (A.map f) xs')
-
-
--- type TuneM a = ReaderT GenIO IO a
-
--- tune :: (Splittable (A.Acc a), Arrays a) => A.Acc a -> TuneM [A.Acc a]
--- tune a = do
---   action <- oneof [minBound .. maxBound]
---   case action of
---     NOP   -> return [a]
---     Force -> return [ A.compute a ]
---     Fizz  -> do
---       s <- oneof splittable
---       p <- splitpoint
---       let (x,y) = splitAt s p a
---       (P.++) <$> tune x <*> tune y
-
--- data Tune = NOP | Force | Fizz
---   deriving (Bounded, Enum, Show)
-
--- splitpoint :: TuneM Double
--- splitpoint = do
---   mwc   <- ask
---   uniformR (0,1) mwc
-
--- oneof :: [a] -> TuneM a
--- oneof these = do
---   mwc  <- ask
---   this <- uniformR (0, P.length these - 1) mwc
---   return (these P.!! this)
--- --}
-
-
--- -- type Tune = IO
-
--- -- eval :: Par a -> Tune [a]
--- -- eval (Do x)    = return [x]
--- -- eval (Par x y) =
-
+repCompose :: (a -> Rep b) -> (b -> Rep c) -> a -> Rep c
+repCompose g f a =
+    case g a of
+      Return a -> f a
+      Bind b f' -> Bind (partialEval (Bind b f')) f
+      Join f' a b -> Bind (partialEval (Join f' a b)) f
+      Use a -> Bind (Use a) f
